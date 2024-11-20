043e7111676bd9e8ba912dd59ceea86bb5c0add0
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2024-11-21 00:11:29 +0300

    BasicCalculator

---
+++ c++/ConsoleApp/ConsoleApp.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int islem;
+	float firsNum, secondNum, result;
+	bool isNext = true;
+
+	while (isNext)
+	{
+
+		cout << "Yapmak istediğiniz işlemi giriniz (Toplama:1,Cikarma:2,Carpma:3,Bolme:4,Cikis:0)" << endl;
+		cin >> islem;
+		if (islem < 1 || islem >= 5)
+		{
+			cout << "Hatali islem girdiniz" << endl;
+		}
+		else
+		{
+			cout << "birinci sayiyi giriniz" << endl;
+			cin >> firsNum;
+
+			cout << "ikinci sayiyi giriniz" << endl;
+			cin >> secondNum;
+		}
+
+		if (islem == 1)
+		{
+			cout << firsNum + secondNum << endl;
+		}
+		else if (islem == 2)
+		{
+			cout << firsNum - secondNum << endl;
+		}
+		else if (islem == 3)
+		{
+			cout << firsNum * secondNum << endl;
+		}
+		else if (islem == 4)
+		{
+			cout << firsNum / secondNum << endl;
+		}
+		else if (islem == 0)
+		{
+			isNext = false;
+			break;
+		}
+	}
+	cout << "İyi Günler Dileriz";
+	return 0;
+}
+
