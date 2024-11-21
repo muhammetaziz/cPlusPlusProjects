69fbd869baed925c8e6a4051708d58d9aebdec35
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2024-11-22 01:55:50 +0300

    22.11.2024

---
+++ c++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//int main()
+//{
+//	int dizi[5] = { 8,9,5,6,7 };
+//	array<int, 5>dizi2 = { 7,4,3,2,1 };
+//	vector<int> dizi3;
+//
+//
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cout << dizi[i] << endl;
+//	}
+//	cout << endl;
+//	for (int i = 0; i < dizi2.size(); i++)
+//	{
+//		cout << dizi2[i] << endl;
+//
+//	}
+//
+//	int ekle;bool devam = true;
+//	while (devam)
+//	{
+//		cout << "sayı gir" << endl;
+//		cin >> ekle;
+//		if (ekle != 0)
+//		{
+//			dizi3.push_back(ekle);
+//		}
+//		else {
+//			cout << "baybay komutanım0" << endl;
+//			devam = false;
+//		}
+//	}
+//	for (int i = 0; i < dizi3.size(); i++)
+//	{
+//		cout << dizi3[i] << endl;
+//
+//	}
+//
+//
+//
+//
+//}
+
+int main() {
+
+	int dizi4[3][3] = { {1,2,3}, {4,5,6},{7,8,9} };
+	 
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int k = 0; k < 3; k++)
+		{
+			cout << dizi4[i][k]<<",";
+
+		}
+		cout << endl;
+	}
+}
--- c++/ConsoleApplication2/ConsoleApplication2.cpp
+++ c++/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,58 +1,71 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
+void ucgen() {
+	int t, h;
+	cout << "Taban degerini giriniz  " << endl;
+	cin >> t;
+	cout << "Yukseklik degerini giriniz  " << endl;
+	cin >> h;
+	cout << (t * h) / 2 << endl;
+}
+void kare() {
+	int k;
+	cout << "Bir kenar uzunlugunu giriniz" << endl;
+	cin >> k;
+	cout << k * k << endl;
+}
+void dikdörtgen() {
+	int k, u;
+	cout << "Kısa kenar ve uzun kenari giriniz" << endl;
+	cin >> k;
+	cout << "Uzun kenari giriniz" << endl;
+	cin >> u;
+	cout << k * u << endl;
+}
+void daire() {
+	int r;
+	cout << "Yaricapi giriniz" << endl;
+	cin >> r;
+	float pi = 3.141592f;
+	cout << pi * pow(r, 2) << endl;
+}
 int main()
 {
-#pragma region deneme
-	/*int userInput, sayac = 0;
-	cout << "Bir Sayı Giriniz" << endl;
-	cin >> userInput;*/
+	int islem = 0;
+	bool devam = true;
+	while (devam)
+	{
+		cout << "Islem seciniz (Ucgen:1 Kare:2 Dikdortgen:3 Daire:4 Cikis:0)" << endl;
+		cin >> islem;
+		switch (islem)
+		{
+		case 1: {
+			ucgen();
+			break;
+		}case 2: {
+			kare();
+			break;
+		}
+		case 3: {
+			dikdörtgen();
+			break;
+		}
+		case 4: {
+			daire();
+			break;
+		}
+		case 0: {
+			cout << "Hoşçakalin" << endl;
+			devam = false;
+			break;
+		}
+		default: {
+			cout << "Geçersiz işlem" << endl;
+			break;
+		}
+		}
+	}
+	return 0;
 
-	//for (int i = 0; i <= userInput; i++)
-	//{
-	//	for (int z = 0; z < i; z++)
-	//	{
-	//		cout << i;
-	//	}
-	//	cout << endl;
-
-	//} 
-	//for (int i = userInput; i > 0; i--)
-	//{
-	//	for (int k = i; k >0; k--)
-	//	{
-
-	//		cout << i ;
-	//	}cout << endl;
-	//}
-#pragma endregion
-#pragma region AsalSayı
-
-	//int userInput, sayac = 0;
-	//cout << "Bir Sayı Giriniz" << endl;
-	//cin >> userInput;
-
-	//for (int i = 1; i < userInput; i++)
-	//{
-	//	if (userInput%i==0)
-	//	{
-	//		 sayac++;
-	//	} 
-	//}
-	//if (sayac<=1)
-	//{
-	//	cout << "sayımız asaldır";
-	//}
-	//else
-	//{
-	//	cout << "sayımız asal değildir" << endl;
-	//}
-#pragma endregion
-	 
-	float result, userInput;
-	cout << "Bir Sayı Giriniz" << endl;
-	cin >> userInput; 
-	result = userInput * 0.18 + userInput;
-	cout << result << endl;
 }
