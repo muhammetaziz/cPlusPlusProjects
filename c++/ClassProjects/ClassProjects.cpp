725e1a97966600a089ea0be7445ca247e20791ae
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2024-12-10 19:39:28 +0300

    *

---
+++ c++/ClassProjects/ClassProjects.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Olasılık
+{
+public:
+	void setSayi(int sayı) {
+		this->sayı = sayı;
+	}
+	int getSayi() {
+		return sayı;
+	}
+private:
+	int sayı;
+	string konum;
+
+};
+
+
+int main()
+{
+	Olasılık ol;
+	cout<<ol.getSayi() << endl;
+	ol.setSayi(12);
+	cout<<ol.getSayi() << endl;
+}
